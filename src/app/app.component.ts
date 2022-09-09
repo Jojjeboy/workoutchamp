22da8dd2436624e9596f90a95ee8edfe4d4a5d7c
Johannes Kärnstam <jokarn@hav.havochvatten.se>
2022-09-09 14:41:32 +0200

    Deploy of the latest and greatest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,15 @@ import { LocalStorageServiceService } from './local-storage-service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  public priceOfWorkoutSubscription:number = 2995;
-  public priceOfWorkoutCard:number = 100;
-  public nrOfWorkoutsCompleted: number = 0;
-  constructor(private localStorageService: LocalStorageServiceService){
 
-  }
+export class AppComponent implements OnInit{
+  private priceOfWorkoutSubscription:number = 2995;
+  private priceOfWorkoutCard:number = 100;
+  private healthCareAllowance:number = 2500;
+  public nrOfWorkoutsCompleted: number = 0;
+  public priceOfEachVisit: number = this.priceOfWorkoutSubscription + this.priceOfWorkoutCard - this.healthCareAllowance;
+
+  constructor(private localStorageService: LocalStorageServiceService){}
 
   ngOnInit(){
     this.getCount();
@@ -20,19 +22,27 @@ export class AppComponent implements OnInit{
 
   public getCount() {
     this.nrOfWorkoutsCompleted = this.localStorageService.getCounter();
+    this.recalculatePrice();
   }
 
   public increase(){
     this.nrOfWorkoutsCompleted += 1;
     this.localStorageService.increase();
+    this.recalculatePrice();
   }
 
   public decrease(){
     if(this.nrOfWorkoutsCompleted > 0){
       this.nrOfWorkoutsCompleted -= 1;
       this.localStorageService.decrease();
+      this.recalculatePrice();
     }
   }
 
-
+  private recalculatePrice(){
+    if(this.nrOfWorkoutsCompleted > 0){
+      this.priceOfEachVisit = (this.priceOfWorkoutSubscription + this.priceOfWorkoutCard - this.healthCareAllowance) / this.nrOfWorkoutsCompleted
+    }
+  }
+  
 }
